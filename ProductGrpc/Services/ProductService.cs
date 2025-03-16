38f93c8282f79d01e16d8dad0ba042c1b2e23b2e
Usman Tahir <Usman.tahir@synergy-it.com>
2025-03-16 15:46:00 +0500

    33. Developing ProductService class to Implement Grpc Proto Service Methods

---
+++ ProductGrpc/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using ProductGrpc.Data;
+using ProductGrpc.Protos;
+using System.Threading.Tasks;
+
+namespace ProductGrpc.Services
+{
+    public class ProductService : ProductProtoService.ProductProtoServiceBase
+    {
+        private readonly ProductsContext _context;
+        private readonly ILogger<ProductService> _logger;
+        public ProductService( ProductsContext context, ILogger<ProductService> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public override Task<Empty> Test(Empty request, ServerCallContext context)
+        {
+            return base.Test(request, context);
+        }
+
+        public override async Task<ProductModel> GetProduct(GetProductRequest request, ServerCallContext context)
+        {
+            var product = await _context.Product.FindAsync(request.ProductId);
+            if (product == null) {
+                throw new System.Exception();
+            }
+            var productModel = new ProductModel
+            {
+                ProductId = request.ProductId,
+                Description = product.Description,
+                Name = product.Name,
+                Price = product.Price,
+                Status = ProductStatus.Instock,
+                CreatedTime = Timestamp.FromDateTime(product.CreatedTime)
+            };
+            return productModel;
+        }
+    }
+} 
