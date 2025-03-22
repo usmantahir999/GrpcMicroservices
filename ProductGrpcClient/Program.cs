eadda73b7d2ea8534586d940e347186499d29383
Usman Tahir <Usman.tahir@synergy-it.com>
2025-03-22 18:07:44 +0500

    38. Refactoring GetAllProducts Server Stream Method with C# 9 Features in Client

--- ProductGrpcClient/Program.cs
+++ ProductGrpcClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using ProductGrpc.Protos;
 using System;
 using System.Threading;
@@ -15,20 +16,40 @@ namespace ProductGrpcClient
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new ProductProtoService.ProductProtoServiceClient(channel);
             //GetProductAsync
-            Console.WriteLine("GetProductAsync started!");
-            var response = await client.GetProductAsync(new GetProductRequest { ProductId = 1 });
-            Console.WriteLine("GetProductAsync response"+response.ToString());
+            await GetProductAsync(client);
 
             //GetAllProductAsync
-            using(var clientData = client.GetAllProducts(new GetAllProductsRequest()))
-            {
-                while(await clientData.ResponseStream.MoveNext(new CancellationToken()))
-                {
-                    var currentProduct = clientData.ResponseStream.Current;
-                    Console.WriteLine(currentProduct);
-                }
-            }
+            await GetAllProductAsync(client);
             Console.ReadLine();
+        }
+
+        private static async Task<AsyncServerStreamingCall<ProductModel>> GetAllProductAsync(ProductProtoService.ProductProtoServiceClient client)
+        {
+            //using(var clientData = client.GetAllProducts(new GetAllProductsRequest()))
+            //{
+            //    while(await clientData.ResponseStream.MoveNext(new CancellationToken()))
+            //    {
+            //        var currentProduct = clientData.ResponseStream.Current;
+            //        Console.WriteLine(currentProduct);
+            //    }
+            //}
+
+            //GetAllProductAsync with c#9
+            Console.WriteLine("GetAllProductAsync started with c#9 !");
+            var clientData = client.GetAllProducts(new GetAllProductsRequest());
+            await foreach (var responseData in clientData.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"{responseData}");
+            }
+
+            return clientData;
+        }
+
+        private static async Task GetProductAsync(ProductProtoService.ProductProtoServiceClient client)
+        {
+            Console.WriteLine("GetProductAsync started!");
+            var response = await client.GetProductAsync(new GetProductRequest { ProductId = 1 });
+            Console.WriteLine("GetProductAsync response" + response.ToString());
         }
     }
 }
