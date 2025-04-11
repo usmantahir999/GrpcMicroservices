70c980857e98c22a1e30dff3a5cc9fddc430f341
Usman Tahir <Usman.tahir@synergy-it.com>
2025-04-11 09:40:55 +0500

    52. Consume Product Grpc Server Method From Product Worker Client Application

--- ProductWorkerService/Worker.cs
+++ ProductWorkerService/Worker.cs
@@ -1,6 +1,8 @@
+using Grpc.Net.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductGrpc.Protos;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -23,8 +25,18 @@ namespace ProductWorkerService
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                using var channel = GrpcChannel.ForAddress(_configuration.GetValue<string>("WorkerService:ServerUrl"));
+                var client = new ProductProtoService.ProductProtoServiceClient(channel);
+                await GetProductAsync(client);
                 await Task.Delay(_configuration.GetValue<int>("WorkerService:TaskInterval"), stoppingToken);
             }
+        }
+
+        private static async Task GetProductAsync(ProductProtoService.ProductProtoServiceClient client)
+        {
+            Console.WriteLine("GetProductAsync started!");
+            var response = await client.GetProductAsync(new GetProductRequest { ProductId = 1 });
+            Console.WriteLine("GetProductAsync response" + response.ToString());
         }
     }
 }
