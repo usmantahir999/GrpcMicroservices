53f37cbd908047ff2657c75e7d5b3e7613d76fe6
Usman Tahir <Usman.tahir@synergy-it.com>
2025-04-12 16:47:12 +0500

    59. Seeding In-Memory Database with Entity Framework Core for ShoppingCartGrpc

--- ShoppingCartGrpc/Program.cs
+++ ShoppingCartGrpc/Program.cs
@@ -1,10 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using ShoppingCartGrpc.Data;
 
 namespace ShoppingCartGrpc
 {
@@ -12,7 +9,9 @@ namespace ShoppingCartGrpc
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
         }
 
         // Additional configuration is required to successfully run gRPC on macOS.
@@ -23,5 +22,13 @@ namespace ShoppingCartGrpc
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void SeedDatabase(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var shoppingCartContext = services.GetRequiredService<ShoppingCartContext>();
+            ShoppingCartContextSeed.SeedAsync(shoppingCartContext);
+        }
     }
 }
