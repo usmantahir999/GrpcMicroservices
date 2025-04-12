bd11a41675919e10f061a132c418690b129dc301
Usman Tahir <Usman.tahir@synergy-it.com>
2025-04-12 17:05:01 +0500

    62. Developing ShoppingCartService class to Implement Grpc Proto Service Methods

---
+++ ShoppingCartGrpc/Services/ShoppingCartService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ShoppingCartGrpc.Data;
+using ShoppingCartGrpc.Protos;
+using System.Threading.Tasks;
+
+namespace ShoppingCartGrpc.Services
+{
+    public class ShoppingCartService : ShoppingCartProtoService.ShoppingCartProtoServiceBase
+    {
+        private readonly ShoppingCartContext _context;
+        private readonly ILogger<ShoppingCartService> _logger;
+        private readonly IMapper _mapper;
+        public ShoppingCartService(ShoppingCartContext context, ILogger<ShoppingCartService> logger, IMapper mapper)
+        {
+            _context = context;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public override async Task<ShoppingCartModel> GetShoppingCart(GetShoppingCartRequest request, ServerCallContext context)
+        {
+            var shoppingCart = await _context.ShoppingCart.FirstOrDefaultAsync(s=>s.UserName ==request.Username);
+            if(shoppingCart == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Shopping cart with username = {request.Username} not found"));
+            }
+            var shoppingCartModel = new ShoppingCartModel();
+            return shoppingCartModel;
+        }
+    }
+}
--- ShoppingCartGrpc/Startup.cs
+++ ShoppingCartGrpc/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ShoppingCartGrpc.Data;
+using ShoppingCartGrpc.Services;
 
 namespace ShoppingCartGrpc
 {
@@ -31,7 +32,7 @@ namespace ShoppingCartGrpc
 
             app.UseEndpoints(endpoints =>
             {
-
+                endpoints.MapGrpcService<ShoppingCartService>();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
