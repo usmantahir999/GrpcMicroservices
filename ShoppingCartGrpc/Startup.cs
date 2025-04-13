6b82ae050dd5c87cdc06943f747a6dbe28eb9f55
Usman Tahir <Usman.tahir@synergy-it.com>
2025-04-13 13:16:14 +0500

    74. Register Discount Grpc Client and Discount Service into the ShoppingCart DI

--- ShoppingCartGrpc/Startup.cs
+++ ShoppingCartGrpc/Startup.cs
@@ -1,22 +1,35 @@
 ﻿using AutoMapper;
+using DiscountGrpc.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ShoppingCartGrpc.Data;
 using ShoppingCartGrpc.Services;
+using System;
 
 namespace ShoppingCartGrpc
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc(); 
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o =>
+            {
+                o.Address = new Uri(Configuration["GrpcConfigs:DiscountUrl"]);
+            });
+            services.AddScoped<DiscountService>();
             services.AddDbContext<ShoppingCartContext>(options =>
                 options.UseInMemoryDatabase("ShoppingCart"));
             services.AddAutoMapper(typeof(Startup));
