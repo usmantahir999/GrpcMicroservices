37893085a802d15771435eef559679a0399de4f1
Usman Tahir <Usman.tahir@synergy-it.com>
2025-04-13 15:16:25 +0500

    85. Configure IdentityServer4 with Adding Config Class for Clients, Resources, Scope

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -16,6 +16,10 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryClients(Config.Clients)
+                .AddInMemoryApiScopes(Config.ApiScopes);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +31,7 @@ namespace IdentityServer
             }
 
             app.UseRouting();
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
