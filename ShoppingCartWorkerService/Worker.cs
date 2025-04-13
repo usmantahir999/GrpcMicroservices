ceaf167a00c890be157ef4f24f4272cbd02c851e
Usman Tahir <Usman.tahir@synergy-it.com>
2025-04-13 14:08:58 +0500

    78. Set Configuration with appsettings.json file into ShoppingCart Worker Service

--- ShoppingCartWorkerService/Worker.cs
+++ ShoppingCartWorkerService/Worker.cs
@@ -1,3 +1,5 @@
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,16 +13,19 @@ namespace ShoppingCartWorkerService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _config;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                using var scChannel = GrpcChannel.ForAddress(_config.GetValue<string>("WorkerService:ShoppingCartServerUrl"));
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
